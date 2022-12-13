655e64dc59087fba81888016c368511345a7fb50
JeffreyScoggins <JeffreyScoggins84@gmail.com>
2022-12-13 18:57:24 -0500

    Cleaned Text formatting, exception for invalid URL

--- OptionsParse.py
+++ OptionsParse.py
@@ -16,6 +16,7 @@ while (True): #infinite loop
         if err.code == 403:
             print ('Invalid Entry')
             continue
+    charLen = len(ticker) - 1 
     datajson = json.loads(data.decode('utf-8')) #Converts to python dictionary
     stockData = datajson["data"] #SubDirectory
     optionsData = stockData["options"] #Subdirectory
@@ -53,9 +54,17 @@ while (True): #infinite loop
             deltaNeutralStrike = greekData['option']
             deltaSumPrevious = deltaSumCurrent
             deltaNeutralStrike = strike
+            if strike[charLen + 12] !='0': #x.xx
+                    deltaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+            elif strike[charLen + 11] !='0': #xx.xx
+                deltaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 11] + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+            elif strike[charLen + 10] !='0':#xxx.xx
+                deltaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
+            else:
+                deltaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 9] + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
         counteven += 2
         countodd += 2
-    print("Delta Neutral: ")
+    print("Delta Neutral:")
     print(deltaNeutralStrike)
 
 
@@ -68,7 +77,7 @@ while (True): #infinite loop
     gammaNeutralStrike = ""
     for x in range(int(len(optionsData)/2)):
 
-        greekData = optionsData[counteven] #Subdirectory
+        greekData = optionsData[counteven] #Subdirect
         greekDataCall = optionsData[counteven]
         greekDataCallDelta = greekDataCall['gamma']
         greekDataPut = optionsData[countodd]
@@ -84,7 +93,16 @@ while (True): #infinite loop
             gammaSumPrevious = greekData['gamma']
             gammaNeutralStrike = greekData['option']
             gammaSumPrevious = gammaSumCurrent
-            gammaNeutralStrike = strike
+            if strike[charLen + 12] !='0': #x.xx
+                gammaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+            elif strike[charLen + 11] !='0': #xx.xx
+               gammaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 11] + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+            elif strike[charLen + 10] !='0':#xxx.xx
+                gammaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
+            else:
+                gammaNeutralStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 9] + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
+
+
         counteven += 2
         countodd += 2
     print("Gamma Neutral: ")
@@ -95,7 +113,7 @@ while (True): #infinite loop
     gammaMax = 0.0
     gammaMaxStrike = ""
 
-    for x in range(300):
+    for x in range(300): #limited options chain to prevent end of chain outliers from skewing data
         greekData = optionsData[count] #Subdirectory
         OI = greekData['open_interest']
         gamma = greekData['gamma']
@@ -104,8 +122,16 @@ while (True): #infinite loop
             gammaMax = gammaMaxTemp
             gammaMaxStrike = greekData['option']
         count += 2
-    print("Gamma Max: ")
-    print (gammaMaxStrike)
+        if strike[charLen + 12] !='0': #x.xx
+            gammaMaxStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+        elif strike[charLen + 11] !='0': #xx.xx
+            gammaMaxStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 11] + strike[charLen + 12] + "." + strike[charLen + 13] + strike[charLen + 14]
+        elif strike[charLen + 10] !='0':#xxx.xx
+            gammaMaxStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
+        else:
+            gammaMaxStrike = strike[charLen + 3] + strike[charLen + 4] + '/' + strike[charLen + 5] + strike[charLen + 6] + '/' + strike[charLen + 1] + strike[charLen + 2] + '  $' + strike[charLen + 9] + strike[charLen + 10] + strike[charLen + 11] + strike[charLen + 12] + '.' + strike[charLen +13] + strike[charLen + 14]
+    print('Gamma Max:')
+    print(gammaMaxStrike)
     
 
                     
